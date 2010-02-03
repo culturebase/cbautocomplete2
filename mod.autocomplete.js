571639b64b26483d7bb7835af3b41b3a6f26ed28
Johannes Wüller <jw@heimat.de>
2010-02-03 11:32:56 +0000

    fix autocomplete

--- mod.autocomplete.js
+++ mod.autocomplete.js
@@ -261,7 +261,7 @@ jQuery.fn.autoComplete = function(params) {
                            }
                         }
                      } else if (options['emptyFooter'] !== false) {
-                        box.find('.__AC_layer').append(options['emptyFooter']);
+                        box.find('.__AC_data').html(options['emptyFooter']);
                      }
                      if (JSONdata['footer'] != undefined) {
                         box.find('.__AC_layer').append(JSONdata['footer']);
