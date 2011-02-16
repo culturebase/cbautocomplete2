a10f30e51136b35eb80d7419e8c4595b3a67245c
Ulf Hermann <ulfhermann@culturebase.org>
2011-02-16 11:57:00 +0000

    move footer freetext functionality into autocomplete2 and simplify selectBoxDataCity.php

--- mod.autocomplete.js
+++ mod.autocomplete.js
@@ -66,6 +66,18 @@ jQuery.fn.autoComplete = function(params) {
        */
       'footer': false,
       /**
+       * Free text entry in footer
+       */
+      'footerFreetext' : false,
+      /**
+       * Description line in case of free text entry in footer
+       */
+      'footerMissingText' : '',
+      /**
+       * Info line in case of free text entry in footer
+       */
+      'footerInfoText' : '',
+      /**
        * Trap "enter" keypress to avoid submitting forms?
        */
       'trapEnter': false
@@ -265,10 +277,76 @@ jQuery.fn.autoComplete = function(params) {
                            }
                         }
                      }
+
+                     var footerFreetext = options['footerFreetext'] || JSONdata['footerFreetext'];
+                     var footerMissingText = options['footerMissingText'] || JSONdata['footerMissingText'];
+                     var footerInfoText = options['footerInfoText'] || JSONdata['footerInfoText'];
                      if (JSONdata['footer'] != undefined) {
                         box.find('.__AC_layer').append(JSONdata['footer']);
                      } else if (options['footer']) {
                         box.find('.__AC_layer').append(jQuery(options['footer']).clone(true));
+                     } else if (footerFreetext) {
+                        box.find('.__AC_layer').append(jQuery("<div>" + footerMissingText + "</div>").click(function(){
+                           jQuery(element).val("");
+                           jQuery("<div><input/><span>go</span></div>").find("span").click(function(){
+                              var name = jQuery(this).parent().find("input").val();
+                              hideBox();
+                              hoverEntry = false;
+                              if (name != "") {
+                                 freetext();
+                                 isFreetext = true;
+                              } else {
+                                 invalidated();
+                                 isFreetext = false;
+                              }
+                              jQuery(element).val(name);
+                              def = typed = name;
+                              jQuery(element).blur();
+                           }).css({
+                              "background-color": "#f0a823",
+                              "color": "#ffffff",
+                              "padding": "2px",
+                              "width": "14px",
+                              "height": "14px",
+                              "display": "block",
+                              "text-align": "center",
+                              "cursor": "pointer",
+                              "margin-left": parseInt(jQuery(".__AC_data").innerWidth()) - 28 + "px"
+                           }).parent().find("input").keyup(function(e){
+                              if (e.which == 13) {
+                                 jQuery(this).parent().find("span").click();
+                              }
+                           }).css({
+                              "border": "0px",
+                              "color": "#c0c0c0",
+                              "display": "block",
+                              "float": "left",
+                              "width": parseInt(jQuery(".__AC_data").innerWidth()) - 30 + "px",
+                              "height": "18px"
+                           }).val(footerInfoText).focus(function() {
+                              jQuery(this).unbind("focus").css({
+                                 "color": "#000000"
+                              }).val("");
+                           }).parent().hover(function() {
+                              hoverEntry = true;
+                           }, function() {
+                              hoverEntry = false;
+                           }).insertAfter(this);
+                           jQuery(this).remove();
+                           jQuery(".__AC_data").remove()
+                        }).hover(function() {
+                           hoverEntry = true;
+                        }, function() {
+                           hoverEntry = false;
+                        }).css({
+                           "color": "#BB3A11",
+                           "background-color": "#F9F5E1",
+                           "margin-right": "10px",
+                           "margin-top": "10px",
+                           "padding": "2px",
+                           "text-align": "right",
+                           "cursor": "pointer"
+                        }));
                      }
                      box.css({
                         'left':   element.offsetLeft + 'px',
